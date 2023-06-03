00731da44f837f306402eab0596c420241a5524e
Adeweh <dorcasabang@gmail.com>
2023-06-03 19:04:35 +0100

    Initial commit

---
+++ src/main/java/com/example/chatgtpbot/ChatgtpBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgtpbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgtpBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgtpBotApplication.class, args);
+	}
+
+}
