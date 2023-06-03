0da43fd62ac84173a9bd270a168e733ecfae7d84
Adeweh <dorcasabang@gmail.com>
2023-06-03 19:28:40 +0100

    updates

--- src/main/java/com/example/chatgtpbot/dto/request/ChatGPTRequest.java
+++ src/main/java/com/example/chatgtpbot/dto/request/ChatGPTRequest.java
@@ -1,4 +1,25 @@
 package com.example.chatgtpbot.dto.request;
 
+import com.example.chatgtpbot.model.Message;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatGPTRequest {
+    private String model;
+    private List<Message> messages;
+
+    public ChatGPTRequest(String model, String prompt) {
+        this.model = model;
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("use", prompt));
+    }
 }
--- src/main/java/com/example/chatgtpbot/model/Message.java
+++ src/main/java/com/example/chatgtpbot/model/Message.java
@@ -1,5 +1,14 @@
 package com.example.chatgtpbot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
     private String role;
     private String content;
